07a48969d0b0fe453fdc6df7529251c556c4338e
chihebbha <chiheb.belhajali@esprit.tn>
2016-04-12 23:07:57 +0200

    hbjbn

--- FootOnClick/models/claim.js
+++
@@ -1,6 +0,0 @@
-var mongoose=require('../config/db');
-var Schema=mongoose.Schema;
-var ClaimSchema=mongoose.Schema({
-    message : string
-});
-module.exports=mongoose.model('reclamation',ClaimSchema);
