ab7fc850c4f68f481e1beb154d67a8752b80c32b
rajajlidi <raja.jlidi@esprit.tn>
2016-04-26 12:15:53 +0100

    commit fb

--- FootOnClick/routes/fb.js
+++ FootOnClick/routes/fb.js
@@ -4,15 +4,15 @@ var router = express.Router();
 var FB = require('fb');
 
 
-FB.options ({'appId' : "1677506365854637"})
-FB.options({ 'appSecret' : '7ea79627746bd10afe9c4ae37c59fef6' })
-FB.options({'appNamespace' : "j'ai pas encore choisi"})
+//FB.options ({'appId' : "675139222625762"})
+//FB.options({ 'appSecret' : '71d199b5dc9820cc135d7ba6f3a3ff44' })
+//FB.options({'appNamespace' : "j'ai pas encore choisi"})
 /* GET users listing. */
 
 
 
 router.get("/data" , function (req, res) {
-    FB.setAccessToken('CAAX1ruMLk60BAN45ccpya2T2fZCtNshpCKfKqVfAj0ogZAGnAvgwfZAEVFP6x9QBQxBfGcNoIFNL6UW4SKNa4e1o8HcTFpPYcu6gtoEPbk0kGsb12BM1mln9ioMXj63IZAAQ1nxhW7RCksJzECx8u6rCnZC0V3MzG7g7GxXUZCY9ceqljNQV1Pr3Bsw18BKfgZD');
+    FB.setAccessToken('CAACEdEose0cBAFUrOHyGVuKKb6t6ZB9V7zotfcnpX6AEdpbibtLjZBrYuZBZBKRZArMi3nSTikbGYsnSQZCWPv17fy8P0rtm5ck5ZCiZChsqAOLQ26CxGcH0Y3NKn97Hr2RjRUJdRD3YQZAQgk1fhFdzEFjxFolUZALsBpr8wXNEx2ZCWZAb5AME4CQvx45EpDCPxzRa0kckYVm4ZBZCqBOHJH9N1C2Bs1QvQJVksZD');
     var rs = [];
     FB.api('/Cristiano?fields=posts{full_picture,comments,message}', function (result) {
         if (!result || result.error) {
